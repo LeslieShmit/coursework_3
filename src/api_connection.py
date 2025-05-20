cb5d4f1d6c3797e9ebea746d99563140e9c08daa
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-05-20 09:18:50 +0400

    add api_connection.py

---
+++ src/api_connection.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+
+import requests
+
+
+class Parser(ABC):
+    """Абстрактный класс для работы с API сервисом"""
+
+    @abstractmethod
+    def _connect_to_api(self, keyword):
+        """Абстрактный метод для подключения к API"""
+        pass
+
+    @abstractmethod
+    def load_vacancies(self, keyword):
+        """Абстрактный метод для получения вакансий по ключевому слову"""
+        pass
+
+
+class HH(Parser):
+    """Класс для работы с API hh.ru"""
+
+    def __init__(self):
+        self.__url = "https://api.hh.ru/vacancies"
+
+    def _connect_to_api(self, keyword):
+        """Приватная функция подключения к API"""
+        params = {"employer_id": keyword, "per_page": 20}
+        response = requests.get(self.__url, params=params)
+        if response.status_code != 200:
+            print(f"Ошибка при обработке запроса: {response.status_code}")
+            return None
+        data = response.json()
+        return data
+
+    def load_vacancies(self, keyword=""):
+        """Функция получает вакансии по id работодателя"""
+        data = self._connect_to_api(keyword)
+        if data and "items" in data:
+            return data["items"]
+        else:
+            return []
