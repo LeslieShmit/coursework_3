894a1495b01a363a6f05ea0d5650d188cfa3b416
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-05-25 19:49:33 +0400

    fixes in db_manager.py and add main.py

--- main.py
+++ main.py
@@ -1,1 +1,69 @@
+
+from config import config
+from src.utils import create_database, save_data_to_database
+from src.api_connection import HH
+from src.db_manager import DBManager
+
 list_of_companies = ["78638", "1740", "3529", "80", "11691810", "11485264", "856498", "8651204", "5685015", "9393023"]
+
+def main():
+    params = config()
+    create_database("vacancies_db", params)
+    for employer in list_of_companies:
+        vacancies = HH().load_vacancies(employer)
+        save_data_to_database(vacancies, "vacancies_db", params)
+    print("База данных была успешно создана.")
+    db_manager = DBManager(database_name="vacancies_db",
+                             user=params["user"],
+                             password=params["password"],
+                             host=params["host"],
+                             port=params["port"])
+    while True:
+        print("Выберете одно из желаемых действий, и введите его номер без точки")
+        print("1. Получить список всех компаний и количество вакансий у каждой компании")
+        print("""2. Получить список всех вакансий с указанием названия компании,
+названия вакансии и зарплаты и ссылки на вакансию""")
+        print("3. Получить среднюю зарплату по вакансиям")
+        print("4. Получить список всех вакансий, у которых зарплата выше средней по всем вакансиям")
+        print("5. Получить список всех вакансий, в названии которых содержится ключевое слово")
+        print("6. Завершение работы")
+        chosen_action = input("Введите номер желаемого действия: ")
+        if chosen_action == "1":
+            data = db_manager.get_companies_and_vacancies_count()
+            for el in data:
+                print(f"{el[0]}: {el[1]}")
+        elif chosen_action == "2":
+            data = db_manager.get_all_vacancies()
+            for el in data:
+                if el[2]:
+                    print(f"{el[1]}. Компания: {el[0]}, зарплата: {el[2]}, ссылка: {el[3]}")
+                else:
+                    print(f"{el[1]}. Компания: {el[0]}, зарплата не указана, ссылка: {el[3]}")
+        elif chosen_action == "3":
+            data = db_manager.get_avg_salary()
+            print(f"Средняя зарплата по вакансиям: {data[0][0]}")
+        elif chosen_action == "4":
+            data = db_manager.get_vacancies_with_higher_salary()
+            for el in data:
+                print(f"{el[1]}. Компания: {el[0]}, зарплата: {el[2]}, ссылка: {el[3]}")
+        elif chosen_action == "5":
+            chosen_keyword = input("Введите ключевое слово для поиска: ")
+            data = db_manager.get_vacancies_with_keyword(chosen_keyword)
+            if data:
+                for el in data:
+                    if el[2]:
+                        print(f"{el[1]}. Компания: {el[0]}, зарплата: {el[2]}, ссылка: {el[3]}")
+                    else:
+                        print(f"{el[1]}. Компания: {el[0]}, зарплата не указана, ссылка: {el[3]}")
+            else:
+                print(f"Вакансий по ключевому слову {chosen_keyword} не найдено")
+        elif chosen_action == "6":
+            db_manager.close_conn()
+            print("Завершение работы")
+            break
+
+
+
+if __name__ == "__main__":
+    main()
+
--- src/db_manager.py
+++ src/db_manager.py
@@ -2,14 +2,18 @@ import psycopg2
 
 class DBManager:
     """Класс для взаимодействия с базой данных"""
-    def __init__(self, database_name, params):
+    def __init__(self, database_name, **params):
         self.database_name = database_name
         self.params = params
+        self.conn = psycopg2.connect(dbname=self.database_name, **self.params)
+
+    def close_conn(self):
+        """Функция для закрытия соединения с базой данных"""
+        self.conn.close()
 
     def get_companies_and_vacancies_count(self):
         """Метод получает список всех компаний и количество вакансий у каждой компании"""
-        conn = psycopg2.connect(dbname=self.database_name, params=self.params)
-        with conn.cursor() as cur:
+        with self.conn.cursor() as cur:
             cur.execute(
                 """
                 SELECT employer_name, COUNT(vacancy_id) AS vacancy_count 
@@ -19,45 +23,38 @@ class DBManager:
                 """
             )
             data = cur.fetchall()
-        conn.close()
         return data
 
 
     def get_all_vacancies(self):
         """Метод получает список всех вакансий с указанием названия компании,
         названия вакансии и зарплаты и ссылки на вакансию"""
-        conn = psycopg2.connect(dbname=self.database_name, params=self.params)
-        with conn.cursor() as cur:
-            with conn.cursor() as cur:
-                cur.execute(
-                    """
-                    SELECT employer_name, vacancy_name, salary_average, vacancy_url
-                    FROM vacancies
-                    LEFT JOIN employers
-                    USING(employer_id)
-                    """
+        with self.conn.cursor() as cur:
+            cur.execute(
+                """
+                SELECT employer_name, vacancy_name, salary_average, vacancy_url
+                FROM vacancies
+                LEFT JOIN employers
+                USING(employer_id)
+                """
                 )
-                data = cur.fetchall()
-            conn.close()
-            return data
+            data = cur.fetchall()
+        return data
 
     def get_avg_salary(self):
         """Метод получает среднюю зарплату по вакансиям"""
-        conn = psycopg2.connect(dbname=self.database_name, params=self.params)
-        with conn.cursor() as cur:
+        with self.conn.cursor() as cur:
             cur.execute(
                 """
                 SELECT ROUND(AVG(salary_average)) FROM vacancies
                 """
             )
             data = cur.fetchall()
-        conn.close()
         return data
 
     def get_vacancies_with_higher_salary(self):
         """Метод получает список всех вакансий, у которых зарплата выше средней по всем вакансиям"""
-        conn = psycopg2.connect(dbname=self.database_name, params=self.params)
-        with conn.cursor() as cur:
+        with self.conn.cursor() as cur:
             cur.execute(
                 """
                 SELECT employer_name, vacancy_name, salary_average, vacancy_url
@@ -68,24 +65,21 @@ class DBManager:
                 """
             )
             data = cur.fetchall()
-        conn.close()
         return data
 
     def get_vacancies_with_keyword(self, keyword):
         """Метод получает список всех вакансий, в названии которых содержатся переданные в метод слова"""
-        conn = psycopg2.connect(dbname=self.database_name, params=self.params)
-        with conn.cursor() as cur:
+        with self.conn.cursor() as cur:
             cur.execute(
                 f"""
                 SELECT employer_name, vacancy_name, salary_average, vacancy_url
                 FROM vacancies
                 LEFT JOIN employers
                 USING(employer_id)
-                WHERE vacancy_name ILIKE '%{keyword}s%'
+                WHERE vacancy_name ILIKE '%{keyword}%'
                 """
             )
             data = cur.fetchall()
-        conn.close()
         return data
 
 
